5652203bf03272b59de6711644e62ed4c4cbf25f
NyanKiyoshi <NyanKiyoshi@users.noreply.github.com>
2016-07-21 01:39:38 +0200

    fast-CGI + NGINX

---
+++ tests/fcgi/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ test_cgi.cpp -lfcgi++ -lfcgi -o $1
---
+++ tests/fcgi/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+APP_NAME="cgi_app.cgi"
+
+# requires "fcgi" and "spawn-fcgi" packages installed
+./compile $APP_NAME && spawn-fcgi -p 8000 -n $APP_NAME
