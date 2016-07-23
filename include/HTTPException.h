2cd337f848de8b05d8f18ea9f9349c808c5a86a1
NyanKiyoshi <NyanKiyoshi@users.noreply.github.com>
2016-07-23 22:46:56 +0200

    Fixed compilation errors.

--- include/HTTPException.h
+++ include/HTTPException.h
@@ -16,143 +16,174 @@ namespace WebPP {
      * The HTTPException and its children will be handled by the application and render a error on the client's browser.
      */
     class HTTPException : public std::exception {
-        protected:
-        virtual int _code;
-        virtual int _description;
+        private:
+        const int _CODE;
+        const int _DESCRIPTION;
 
         public:
+        /*
+         * A blueprint could easily handle a HTTPException and for example generate a JSON response instead of
+         * a HTML one. Or anything else without necessarily change/ inherit the HTTPException class.
+         */
         void get_body(char *buffer) const;
         void get_headers(char *buffer) const;
-
         Response generated_response() const;
 
         int get_code() const {
-            return this->_code;
+            return this->_CODE;
         }
 
         int get_description() const {
-            return this->_description;
+            return this->_DESCRIPTION;
         }
     };
 
 
     class BadRequest : public HTTPException {
+        int _CODE = 400;
     };
 
 
-    class ClientDisconnected : public HTTPException {
+    class ClientDisconnected : public BadRequest {
     };
 
 
-    class SecurityError : public HTTPException {
+    class SecurityError : public BadRequest {
     };
 
 
-    class BadHost : public HTTPException {
+    class BadHost : public BadRequest {
+        int _CODE = 401;
     };
 
 
     class Unauthorized : public HTTPException {
+        int _CODE = 401;
     };
 
 
     class Forbidden : public HTTPException {
+        int _CODE = 403;
     };
 
 
     class NotFound : public HTTPException {
+        int _CODE = 404;
     };
 
 
     class MethodNotAllowed : public HTTPException {
+        int _CODE = 405;
     };
 
 
     class NotAcceptable : public HTTPException {
+        int _CODE = 406;
     };
 
 
     class RequestTimeout : public HTTPException {
+        int _CODE = 408;
     };
 
 
     class Conflict : public HTTPException {
+        int _CODE = 409;
     };
 
 
     class Gone : public HTTPException {
+        int _CODE = 410;
     };
 
 
     class LengthRequired : public HTTPException {
+        int _CODE = 411;
     };
 
 
     class PreconditionFailed : public HTTPException {
+        int _CODE = 412;
     };
 
 
     class RequestEntityTooLarge : public HTTPException {
+        int _CODE = 413;
     };
 
 
     class RequestURITooLarge : public HTTPException {
+        int _CODE = 414;
     };
 
 
     class UnsupportedMediaType : public HTTPException {
+        int _CODE = 415;
     };
 
 
     class RequestedRangeNotSatisfiable : public HTTPException {
+        int _CODE = 416;
     };
 
 
     class ExpectationFailed : public HTTPException {
+        int _CODE = 417;
     };
 
 
     class ImATeapot : public HTTPException {
+        int _CODE = 418;
     };
 
 
     class UnprocessableEntity : public HTTPException {
+        int _CODE = 422;
     };
 
 
     class PreconditionRequired : public HTTPException {
+        int _CODE = 428;
     };
 
 
     class TooManyRequests : public HTTPException {
+        int _CODE = 429;
     };
 
 
     class RequestHeaderFieldsTooLarge : public HTTPException {
+        int _CODE = 431;
     };
 
 
     class InternalServerError : public HTTPException {
+        int _CODE = 500;
     };
 
 
     class NotImplemented : public HTTPException {
+        int _CODE = 501;
     };
 
 
     class BadGateway : public HTTPException {
+        int _CODE = 502;
     };
 
 
     class ServiceUnavailable : public HTTPException {
+        int _CODE = 503;
     };
 
 
     class GatewayTimeout : public HTTPException {
+        int _CODE = 504;
     };
 
 
     class HTTPVersionNotSupported : public HTTPException {
+        int _CODE = 505;
     };
 }
 #endif //WEBPP_HTTPEXCEPTION_H
