2275200325bdf14284fd2275c55eb972db0a2c1d
NyanKiyoshi <NyanKiyoshi@users.noreply.github.com>
2016-07-23 19:36:15 +0200

    Few improvements and security measures to get a given header from the environment.

--- include/Request.h
+++ include/Request.h
@@ -7,6 +7,7 @@
 
 
 #include <fcgiapp.h>
+#include <string.h>
 
 
 namespace WebPP {
@@ -15,17 +16,24 @@ namespace WebPP {
      */
     class Request {
         private:
+        const char* ENV_HEADER_PREFIX = "HTTP_";
+        const size_t PREFIX_LENGTH = strlen(ENV_HEADER_PREFIX);
+
         const FCGX_Request &REQUEST;
+
+        inline char* get_from_env(const char *name);
         void find_associated_route();
 
         public:
         Request(FCGX_Request &request);
 
-        char *request_method();
-        char *raw_query_string();
-        char *raw_cookies();
-        char *user_agent();
-        char *host();
+        char* get_header(const char* header_name);
+
+        char* request_method();
+        char* raw_query_string();
+        char* raw_cookies();
+        char* user_agent();
+        char* host();
     };
 }
 
--- src/Request.cpp
+++ src/Request.cpp
@@ -2,6 +2,7 @@
 // Created by sayuri on 7/20/16.
 //
 
+#include <cstring>
 #include "Request.h"
 
 
@@ -9,12 +10,26 @@ WebPP::Request::Request(FCGX_Request &request) : REQUEST(request) {
 
 }
 
+char* WebPP::Request::get_header(const char* header_name) {
+    size_t header_length = strlen(header_name);
+    char new_header_name[this->PREFIX_LENGTH + header_length];
+
+    strcat(new_header_name, this->ENV_HEADER_PREFIX);
+    strcat(new_header_name, header_name);
+
+    return FCGX_GetParam(new_header_name, this->REQUEST.envp);
+}
+
+inline char *WebPP::Request::get_from_env(const char *name) {
+    return FCGX_GetParam(name, this->REQUEST.envp);
+}
+
 char* WebPP::Request::request_method() {
-    static char* request_method = FCGX_GetParam("HTTP_REQUEST_METHOD", this->REQUEST.envp);
-    return request_method;
+    static char* header = this->get_from_env("HTTP_REQUEST_METHOD");
+    return header;
 }
 
 char* WebPP::Request::user_agent() {
-    static char* user_agent = FCGX_GetParam("HTTP_USER_AGENT", this->REQUEST.envp);
-    return user_agent;
+    static char* header = this->get_from_env("HTTP_USER_AGENT");
+    return header;
 }
